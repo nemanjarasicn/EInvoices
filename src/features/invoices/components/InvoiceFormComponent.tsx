39f6f4221a33b8aa94b043f5ec5d4759e956670f
ztramp <ztrampic@yahoo.com>
2022-10-24 21:32:17 +0200

    added action buttons

--- src/features/invoices/components/InvoiceFormComponent.tsx
+++ src/features/invoices/components/InvoiceFormComponent.tsx
@@ -11,6 +11,7 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  IconButton,
 } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { InvoiceFormModel, InvoiceType, IProps } from "../models";
@@ -28,6 +29,9 @@ import FormCheckboxField from "./form-fields/FormCheckboxField";
 import FormCurrencyField from "./form-fields/FormCurrencyField";
 import PrepaymentComponent from "./form-group/PrepaymentComponent";
 import InvoiceGroupComponent from "./form-group/InvoiceGroupComponent";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import RestoreRoundedIcon from "@mui/icons-material/RestoreRounded";
+import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
 import { error } from "console";
 import {
   OptionItem,
@@ -330,7 +334,56 @@ export default function InvoiceFormComponent({
           </Box>
         </Grid>
         <Grid item xs={2}>
-          Actions
+          <Grid item xs={12}>
+            <IconButton
+              color="primary"
+              aria-label="TextsmsOutlinedIcon"
+              size="large"
+              disabled
+              sx={{
+                backgroundColor: "#f5f5f5",
+                "&:disabled": {
+                  backgroundColor: "#f5f5f5"
+                }
+              }}
+            >
+              <TextsmsOutlinedIcon />
+            </IconButton>
+          </Grid>
+          <br />
+          <Grid item xs={12}>
+            <IconButton
+              color="primary"
+              aria-label="AttachFileIcon"
+              size="large"
+              disabled
+              sx={{
+                backgroundColor: "#f5f5f5",
+                "&:disabled": {
+                  backgroundColor: "#f5f5f5"
+                }
+              }}
+            >
+              <AttachFileIcon />
+            </IconButton>
+          </Grid>
+          <br />
+          <Grid item xs={12}>
+            <IconButton
+              color="primary"
+              aria-label="RestoreRoundedIcon"
+              size="large"
+              disabled
+              sx={{
+                backgroundColor: "#f5f5f5",
+                "&:disabled": {
+                  backgroundColor: "#f5f5f5"
+                }
+              }}
+            >
+              <RestoreRoundedIcon />
+            </IconButton>
+          </Grid>
         </Grid>
       </Grid>
       <Grid container spacing={2}>
